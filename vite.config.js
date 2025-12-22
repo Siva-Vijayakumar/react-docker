dfa0ae441a8f3ce0b70003597de9a8dcf01ee21f
Siva V <sivavijayakumar1222@gmail.com>
2025-12-22 11:16:16 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  cacheDir: '/tmp/.vite',
+
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+
+    watch: {
+      usePolling: true,
+      interval: 100
+    },
+
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      port: 5173
+    }
+  }
+})
